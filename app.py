c4b2e3c0e9f8a4f9f5d9d5abd3ff170ab0d3d03c
KhalHajji <48171312+KhalHajji@users.noreply.github.com>
2023-02-06 14:42:03 -0500

    app-2

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
-# from flask import Flask, render_template
+from flask import Flask, render_template
 
-# app = Flask(__name__)
-
-# @app.route("/")
-# def index():
-#     return render_template('index.html', message="Hello World..!!")
-
-# if __name__ == '__main__':
-#     app.run(host="0.0.0.0", port=5000, debug=True)
-
-#!/usr/bin/env python
-from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/hello/')
-def hello_world():
-    return 'Hello World!\n'
-
-@app.route('/hello/<username>') # dynamic route
-def hello_user(username):
-    return 'Why Hello %s!\n' % username
+@app.route("/")
+def index():
+    return render_template('index.html', message="Hello World..!!")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')     # open for everyone
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
+
+# #!/usr/bin/env python
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/')
+# @app.route('/hello/')
+# def hello_world():
+#     return 'Hello World!\n'
+
+# @app.route('/hello/<username>') # dynamic route
+# def hello_user(username):
+#     return 'Why Hello %s!\n' % username
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0')     # open for everyone
